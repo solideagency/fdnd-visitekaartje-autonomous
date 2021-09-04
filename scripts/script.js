e27422698431146eb69a203a479f7792c255ec7b
solideagency <suus@solide.agency>
2021-09-04 19:01:23 +0200

    responsive and sticky note

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,16 @@
 //switch
 var switchButton = document.querySelector('.switch input');
+var text = document.querySelector('header > h1');
 var bodyBackground = document.querySelector('body');
 switchButton.addEventListener('change', function(e){
   if(switchButton.checked === true){
     bodyBackground.classList.add("barcelona");
     bodyBackground.classList.remove("amsterdam");
-    document.querySelector('.city-images .amsterdam').style.display = "none";
-    document.querySelector('.city-images .barcelona').style.display = "block";
+    text.innerHTML = "Barcelona";
   } else {
     bodyBackground.classList.remove("barcelona");
     bodyBackground.classList.add("amsterdam");
-    document.querySelector('.city-images .amsterdam').style.display = "block";
-    document.querySelector('.city-images .barcelona').style.display = "none";
+    text.innerHTML = "Amsterdam";
+
   }
 })
