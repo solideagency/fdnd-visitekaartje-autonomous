ce575cbe3990a0ea9cef51e7ae73a4b25ef0d561
solideagency <suus@solide.agency>
2022-09-04 20:15:10 +0200

    more simplistic style

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,16 @@
 //switch
 var switchButton = document.querySelector('.switch input');
-var text = document.querySelector('header > h1');
+// var text = document.querySelector('header > h1');
 var bodyBackground = document.querySelector('body');
 switchButton.addEventListener('change', function(e){
   if(switchButton.checked === true){
     bodyBackground.classList.add("barcelona");
     bodyBackground.classList.remove("amsterdam");
-    text.innerHTML = "Barcelona";
+    // text.innerHTML = "Barcelona";
   } else {
     bodyBackground.classList.remove("barcelona");
     bodyBackground.classList.add("amsterdam");
-    text.innerHTML = "Amsterdam";
+    // text.innerHTML = "Amsterdam";
 
   }
 })
